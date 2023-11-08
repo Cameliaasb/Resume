d786eb06f17d9039728c6288ef86b033041e6786
camelia assobhei <camelia.assobhei@gmail.com>
2023-11-08 12:52:51 +0100

    [BACK] Added routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React    from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App      from './App';
+import Navbar   from './components/Navbar';
+import          './styles/index.scss';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
+    <Navbar />
     <App />
   </React.StrictMode>
 );
---
+++ src/pages/Resume.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <div>
+      <p>RESUME a faire avec react-pdf</p>
+    </div>
+  );
+};
+
+export default Resume;
