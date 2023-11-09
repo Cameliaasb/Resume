64f7c05a2c5c105f7389d3ecd366971bdbe8076c
camelia assobhei <camelia.assobhei@gmail.com>
2023-11-09 13:04:50 +0100

    [FRONT] Bio

--- src/pages/Bio.js
+++ src/pages/Bio.js
@@ -1,31 +1,37 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGraduationCap } from '@fortawesome/free-solid-svg-icons';
+import { faBolt } from '@fortawesome/free-solid-svg-icons';
+
 const Bio = () => {
   return (
 
     <div>
       <Navbar />
+      <div className="two-cols">
+        <div className="mask">
+          <img src="picture.jpg" alt="resume" width="320px" className="cover" />
 
-      <div>
-        <p>Hi, I'm Camelia Assobhei, a Backend Engineer with a passion for web development.  </p>
-        <p>
-          I transitioned from a career as a financial analyst after attending Le Wagon's web development bootcamp.
-          My journey reflects my commitment to continuous learning and growth.
-        </p>
-        <p>
-          During my time in the financial industry, I worked as a Private Equity Analyst at both Capelia Management and Naxicap Partners.
-          I gained experience in prospecting for investment targets, financial analysis, and preparing investment memos.
-        </p>
-        <p>
-          I hold a Master's in Finance from ESCP Europe and have international exchange experiences in Sweden and the United States.
-          Fluent in several languages, I bring a diverse perspective to my work.
-        </p>
-        <p>
-          My goal is to continue learning and becoming a better developer.
-          I'm enthusiastic about web development and excited to bring my unique skill set to every project I undertake.
-        </p>
-        <p> Feel free to connect with me on LinkedIn or explore my code on GitHub to learn more about my journey and experiences.
-        </p>
+        </div>
+
+        <div className='text'>
+          <h2> I'm Camelia, a Full-stack Developer.  </h2>
+
+          <p>
+            Transitioned from a career as a financial analyst after attending <b><a href="https://www.lewagon.com/fr" target="_blank" rel="noopener noreferrer">Le Wagon</a></b>'s web development bootcamp with a newfound passion for coding.
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faGraduationCap} className="blue"/> ESCP Europe and La Sorbonne graduate with enriching international exchange experiences.
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faBolt} className="blue"/> Passionate about continuous learning and bringing a unique skill set to every project.
+          </p>
+
+          <p>
+            Feel free to connect with me on <b><a href="https://www.linkedin.com/in/cameliaasb/" target="_blank" rel="noopener noreferrer">LinkedIn</a></b> or explore my code on <b><a href="https://github.com/Cameliaasb" target="_blank" rel="noopener noreferrer">GitHub</a></b> to learn more about my journey and experiences.
+          </p>
+        </div>
       </div>
     </div>
   );
