27cc6a4c93408afe105201aaf4c5b534e895374e
camelia assobhei <camelia.assobhei@gmail.com>
2023-11-10 10:25:46 +0100

    [FRONT] Final color palette / no navbar overflow

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,25 +9,27 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 const Navbar = () => {
 
   return (
-    <div className="navbar">
-      <h3> Camelia </h3>
+    <div className='navbar-container'>
+      <div className="navbar">
+        <h3> <NavLink to="/"> Camelia </NavLink></h3>
 
-      <div className='links'>
-        <NavLink to="/bio"        className={(nav) => (nav.isActive? "nav-active" : "")} > BIO       </NavLink>
-        <NavLink to="/projects"   className={(nav) => (nav.isActive? "nav-active" : "")} > PROJECTS  </NavLink>
-        <NavLink to="/resume"     className={(nav) => (nav.isActive? "nav-active" : "")} > RESUME    </NavLink>
-        <NavLink to="/contact"    className={(nav) => (nav.isActive? "nav-active" : "")} > CONTACT   </NavLink>
+        <div className='links'>
+          <NavLink to="/"           className={(nav) => (nav.isActive? "nav-active" : "")} > BIO       </NavLink>
+          <NavLink to="/projects"   className={(nav) => (nav.isActive? "nav-active" : "")} > PROJECTS  </NavLink>
+          <NavLink to="/resume"     className={(nav) => (nav.isActive? "nav-active" : "")} > RESUME    </NavLink>
+          <NavLink to="/contact"    className={(nav) => (nav.isActive? "nav-active" : "")} > CONTACT   </NavLink>
+        </div>
+
+        <div className="links">
+          <a href="https://www.linkedin.com/in/cameliaasb/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a href="https://github.com/Cameliaasb" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </div>
+
       </div>
-
-      <div className="links">
-        <a href="https://www.linkedin.com/in/cameliaasb/" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faLinkedin} />
-        </a>
-        <a href="https://github.com/Cameliaasb" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faGithub} />
-        </a>
-      </div>
-
     </div>
   );
 };
