d3cf1807a153300e13b5fa3de109e0929abfd7d1
camelia assobhei <camelia.assobhei@gmail.com>
2023-11-10 10:26:20 +0100

    [BUGFIX] Babel dependency / React in component

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -1,4 +1,4 @@
-import { useEffect, useState }  from 'react';
+import React, { useEffect, useState }  from 'react';
 import { callGithubAPI }        from './GithubAPI';
 
 const Languages = ({ url }) => {
