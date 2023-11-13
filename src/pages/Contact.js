64f16503de0656fba885bc8c92f3855bab3729ca
camelia assobhei <camelia.assobhei@gmail.com>
2023-11-13 10:42:28 +0100

    [UX] Contact page with copy btn

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faEnvelope, faCheck, faCopy } from '@fortawesome/free-solid-svg-icons';
+
 
 const Contact = () => {
+  const [copied, setCopied] = useState(false);
+
+  const copy = () => {
+    navigator.clipboard.writeText("camelia.assobhei@gmail.com");
+    setCopied(true)
+  }
   return (
     <div>
-    <Navbar />
+      <Navbar />
 
-    <div class="container">
-    <h2>Contact Me</h2>
+      <div class="container">
+        <h2>Contact Me</h2>
 
-    <div class="contact-info">
-        <p>Email: <a href="mailto:camelia.assobhei@gmail.com">camelia.assobhei@gmail.com</a></p>
-        <p>Phone: 07 60 26 37 60</p>
-    </div>
-</div>
+        <div class="contact-infos">
+          <div className='contact-info'>
+            <FontAwesomeIcon icon={faEnvelope} className="blue" /> <a href="mailto:camelia.assobhei@gmail.com">camelia.assobhei@gmail.com</a>
+
+            <div className='copy-btn blue' onClick={copy}>
+              <p> {copied ? "Copied" : ""} <FontAwesomeIcon icon={copied ? faCheck : faCopy} /></p>
+            </div>
+          </div>
+
+          <div className='contact-info'><FontAwesomeIcon icon={faPhone} className="blue" /> 07 60 26 37 60</div>
+        </div>
+      </div>
     </div>
   );
 };
