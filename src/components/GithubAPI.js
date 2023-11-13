20825273e7b922aff6018edcce30df38e60977bb
camelia assobhei <camelia.assobhei@gmail.com>
2023-11-13 11:36:00 +0100

    [BUGFIX] Upgraded dependencies

--- src/components/GithubAPI.js
+++ src/components/GithubAPI.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
 export const callGithubAPI = (url) => {
+  const key = process.env.REACT_APP_GH.replace("?", "")
+
   return axios.get(url, {
     headers: {
-      Authorization: 'Bearer '+process.env.REACT_APP_GH,
+      Authorization: 'Bearer '+ key,
       'X-GitHub-Api-Version': "2022-11-28",
     },
-  });
+  }
+  );
 };
